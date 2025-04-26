e62025ae190425157e4efecefd968747c77023f8
Leon Braungardt <leonbraungardt@t-online.de>
2025-04-26 12:49:19 +0200

    * improve comments * refactor of frame read loop in __next__()

--- spi_ftdi_frame_reader.py
+++ spi_ftdi_frame_reader.py
@@ -15,47 +15,39 @@ class SpiFtdiFrameReader:
     """
     A SPI data reader for reading data from a IWRL6432BOOST via C232HM-DDHSL-0 FTDI USB cable
     to a host computer. It uses the SPI_BUSY GPIO pin on the IWRL6432BOOST for synchronization.
-    Repeatedly reads frame_length number of Bytes when SPI_BUSY changes to low state.
-
-    Attributes:
-        frame_length (int):
-            Number of bytes that each call to `__next__()` will return. Must match the frame length
-            set in the MCU (radar cube size or ADC samples size of one frame).
-        url (str):
-            FTDI-URI string, e.g. "ftdi://ftdi:232h/1?latency=1". Refer to PyFtdi docs and supported
-            devices for this parameter.
-        cs (int):
-            Which chip‐select line to use.
-        freq (float):
-            SPI clock frequency, in Hz.
-        mode (int):
-            SPI mode (0–3).
-        max_chunk_size (int):
-            Maximum number of bytes the FTDI chip can transfer in one operation.
-    
-    Usage:
-        reader = RadarCubeReader(num_tx_antennas=2,
-                        num_rx_antennas=3,
-                        num_range_bins=64,
-                        num_chirps_per_frame=8,
-                        spi_uri="ftdi://ftdi:232h/1?latency=1")
-
-        for cube in reader:
-            print(cube.data)
-
+    Repeatedly reads frame_length number of Bytes in chunks, each chunk is read when SPI_BUSY 
+    changes to low state.
     """
     def __init__(self,
                 frame_length: int,
                 uri: str = "ftdi://ftdi:232h/1?latency=1",
                 cs: int = 0,
-                freq: float = 15e6,
+                freq: float = 30e6,
                 mode: int = 0,
-                max_chunk_size: int = 64500,
+                max_chunk_size: int = 65024,
                ):
+        """
+        Args:
+            frame_length (int)  : Number of bytes that each call to `__next__()` will return. Must match the frame length
+                                    set in the MCU (radar cube size or ADC samples size of one frame).
+            uri: (str)          : SPI FTDI URI (defaults to ftdi://ftdi:232h/1?latency=1)
+            cs: (int)           : SPI Chip Select (defaults to 0)
+            freq: (float)       : SPI frequency in Hz (defaults to 30e6 = 30 MHz)
+            mode: (int)         : SPI mode (defaults to 0)
+            max_chunk_size: (int): SPI max chunk size the SPI USB chip supports in one transfer, 
+                                    minus required overhead (defaults to 65024). 
+                                    Must be divisable by 4.
+
+        Raises:
+            UsbToolsError: If PyFtdi is unable to open the FTDI device with the supplied URI.
+            RuntimeError: If FTDI GPIO cannot be initialized
+        """
+        if max_chunk_size % 4 != 0:
+            raise ValueError("max_chunk_size must be divisible by 4 (datasize in Bytes of SPI transaction).")
 
         self.frame_length   = frame_length
         self.max_chunk_size = max_chunk_size
-        self._spi           = SpiController()
+        self._spi           = SpiController(turbo=True)
         self._gpio_port: GpioPort = None
 
         try:
@@ -84,40 +76,56 @@ class SpiFtdiFrameReader:
         'for frame in reader' continuously reads new frames synchronized by the SPI_busy pin.
         Waits for the SPI_busy pin (ADBUS4) to go low before reading a frame.
         """
-        # poll _gpio_port to check if it has gone low, indicating that data can be read
-        while True:
-            try:
-                gpio_state = self._gpio_port.read()
-                if (gpio_state & SPI_BUSY_PIN_MASK) == 0:
-                    # pin is low, data is ready to be read
-                    break
-            except Exception as e:
-                 print(f"Error during busy wait: {e}")
-                 raise StopIteration from e
+        # read the entire frame, splitting into chunks if the frame_length exceeds max_chunk_size
+        remaining_bytes = self.frame_length
+        frame_data = bytearray()
 
-
-        # once the SPI_BUSY signal is low, read the expected frame length
         try:
-            # read the entire frame, splitting into chunks if the frame_length exceeds max_chunk_size
-            remaining_bytes = self.frame_length
-            frame_data = bytearray()
             while remaining_bytes > 0:
-                chunk_size = min(remaining_bytes, self.max_chunk_size)
-                # read chunk of data from the SPI port
-                chunk = self._port.read(chunk_size)
-                if not chunk:
-                    print(f"Error: No data received after SPI_BUSY signal changed to low, expected {chunk_size} bytes.")
-                    raise StopIteration
-                frame_data.extend(chunk)
-                remaining_bytes -= len(chunk)
+                # poll _gpio_port to check if it has gone low, indicating that a chunk of data can be read
+                while True:
+                    try:
+                        gpio_state = self._gpio_port.read()
+                        if (gpio_state & SPI_BUSY_PIN_MASK) == 0:
+                            # pin is low, data is ready to be read
+                            break
+                    except Exception as e:
+                        print(f"Error during busy wait: {e}")
+                        raise StopIteration from e
+                    
+                try:
+                    # once the SPI_BUSY signal is low, read the expected chunk length
+                    chunk_size = min(remaining_bytes, self.max_chunk_size)
+                    chunk = self._port.read(chunk_size)
+                    if not chunk:
+                        print(f"Error: No data received after SPI_BUSY signal changed to low, expected {chunk_size} bytes.")
+                        raise StopIteration
+                except Exception as e:
+                    print(f"Error during chunk read: {e}")
+                    raise StopIteration from e
 
-            # return the full frame
-            return frame_data
+                # Each set of 4 Byte arrives in Byte order [Byte_D, Byte_C, Byte_B, Byte_A], so order needs to be switched
+                # before adding it to frame_data (not the most elegant solution)
+                for i in range(0, chunk_size, 4):
+                    byte_D = chunk[i]
+                    byte_C = chunk[i + 1]
+                    byte_B = chunk[i + 2]
+                    byte_A = chunk[i + 3]
 
+                    frame_data.append(byte_A)
+                    frame_data.append(byte_B)
+                    frame_data.append(byte_C)
+                    frame_data.append(byte_D)
+
+                remaining_bytes -= chunk_size
         except Exception as e:
-            # catch other potential errors during read
             print(f"Error during frame read: {e}")
             raise StopIteration from e
+
+        # return the full frame
+        return frame_data
+
+        
 
     def close(self) -> None:
         """
