446ca2ae233bb28d493204e4be8069797f4cb440
Leon Daniel Braungardt <leon_daniel.braungardt@smail.th-koeln.de>
2025-05-14 15:40:29 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name="mmwave-spi-ftdi-reader",
     version="0.1.0",
-    description="Unofficial SPI via FTDI USB cable data reader Python module for TI mmWave sensors",
+    description="An unofficial Python library for streaming and parsing 1D Radar Cube data from TI mmWave sensors via SPI using an FTDI adapter",
     author="Leon Braungardt",
     packages=find_packages(exclude=["examples", "images"]),
     install_requires=[
         "numpy",
-        "pyftdi"
+        "pyftdi",
+        "xarray"
     ],
     python_requires=">=3.10",
 )
